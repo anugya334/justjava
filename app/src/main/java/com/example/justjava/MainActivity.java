14493c683659ac5044a8653477488b97983aaffe
anugya <anugyaupadhayay@gmai.com>
2020-04-27 16:47:16 +0530

    final commit

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -6,10 +6,13 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.NumberFormat;
+
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    int numberOfCoffee = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +23,21 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder(View view) {
-        display(3);
+    public void increment(View view) {
+        numberOfCoffee = numberOfCoffee + 1;
+        display(numberOfCoffee);
     }
+
+    public void decrement(View view) {
+        numberOfCoffee = numberOfCoffee - 1;
+        display(numberOfCoffee);
+    }
+
+    public void submitOrder(View view) {
+
+        displayPrice(numberOfCoffee * 5);
+    }
+
 
     /**
      * This method displays the given quantity value on the screen.
@@ -31,4 +46,10 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
 }
